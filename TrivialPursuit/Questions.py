beade8cf95c9d6a5d06e5f2a67eeaaa0705fa79f
naumanmj <naumanmj@gmail.com>
2015-04-30 18:44:31 -0500

    Add questions

--- TrivialPursuit/Questions.py
+++ TrivialPursuit/Questions.py
@@ -16,7 +16,9 @@ None at this time
 # Dependencies
 ################################################################################
 
+import csv
 import pymsgbox
+import random
 
 ################################################################################
 # Variables
@@ -57,7 +59,37 @@ class Questions:
         """
 
         # Probably need to read/initialize the questions here
-        pass
+        self.questions = {}
+        input_questions = csv.reader(open("questions.csv", "rb"))
+
+        # Loop through all of the questions
+        first = True
+        for (category, question, correct, incorrect_1,
+             incorrect_2, incorrect_3) in input_questions:
+
+            # This is not the header row
+            if first is False:
+
+                # Add this category, if necessary
+                if category not in self.questions:
+                    self.questions[category] = []
+
+                # Add this question
+                self.questions[category].append([question,
+                                                 [[correct, True],
+                                                  [incorrect_1, False],
+                                                  [incorrect_2, False],
+                                                  [incorrect_3, False]]])
+
+            # Mark the header row processed
+            else:
+                first = False
+
+        # Shuffle the deck
+        self.location = {}
+        for category in self.questions.keys():
+            self.location[category] = 0
+            random.shuffle(self.questions[category])
 
     def ask(self, question_type):
         """
@@ -74,11 +106,46 @@ class Questions:
         True/False based on if the question is answered correctly
         """
 
-        # Ask a dummy question for now
-        answer = pymsgbox.confirm("Is this a Question?", question_type, ["Yes", "No"])
-        if answer == "Yes":
+        # Get the next question
+        index = self.location[question_type]
+        self.location[question_type] += 1
+        if self.location[question_type] >= len(self.questions[question_type]):
+            self.location[question_type] = 0
+
+        # Shuffle the answers
+        random.shuffle(self.questions[question_type][index][1])
+
+        # Generate the question text
+        question = "%s\n\n" % (self.questions[question_type][index][0])
+
+        # Generate the answer text
+        number = 1
+        correct = 1
+        for answer in self.questions[question_type][index][1]:
+
+            # Add this answer option
+            question += "(%d) %s\n" % (number, answer[0])
+
+            # If this is the correct answer, store
+            if answer[1]:
+                correct = number
+
+            # Move next answer option
+            number += 1
+
+        # Ask the question
+        answer = pymsgbox.confirm(question, question_type,
+                                  ["1", "2", "3", "4"])
+
+        # If answered correctly, assert true
+        if int(answer) == correct:
+            pymsgbox.alert("Correct", "Congratulations")
             return True
+
+        # If answered incorrectly, assert false
         else:
+            correct_text = self.questions[question_type][index][1][correct-1][0]
+            pymsgbox.alert("Incorrect: %s" % correct_text, "Sorry")
             return False
 
 ################################################################################
